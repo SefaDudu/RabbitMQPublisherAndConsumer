2a7970addc143d4fd0d01a46b6d6ead25ea68e48
Sefa Dudu <sefa.dudu@rsu.com.tr>
2023-02-09 17:17:02 +0300

    Add project files.

---
+++ Consumer/Program.cs
@@ -0,0 +1,30 @@
+﻿//Bağlantı oluşturma
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+
+//bağlantıyı aktifleştirme ve kanal açma
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+//Queue oluşturma --  yapılandırma publisher ile aynı olmalı
+channel.QueueDeclare(queue: "example-queue", exclusive: false);
+
+
+//Queue mesaj okuma
+
+EventingBasicConsumer consumer = new(channel);
+channel.BasicConsume(queue: "example-queue",autoAck:false,consumer);
+consumer.Received += (sender, e) =>
+{
+    //e.Body : mesajın verisini getirir bütünsel olarak.
+    //e.Body.Span veya toArray mesajın byte verisini getirir.
+
+    //kuyruğa gelen mesajın işlendiği yer burasıdır.
+    
+    Console.WriteLine(Encoding.UTF8.GetString(e.Body.Span));
+
+};
+Console.Read();
---
+++ Publisher/Program.cs
@@ -0,0 +1,26 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+//Bağlantı oluşturma
+var factory = new ConnectionFactory { HostName = "localhost" };
+
+//bağlantıyı aktifleştirme ve kanal açma
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+//Queue oluşturma
+channel.QueueDeclare(queue: "example-queue",exclusive:false);
+
+//Queue'ya mesaj gönderme
+//Rabbitmq kuyruğa atacağı mesajları byte türünden kabul eder. mesajları byte'a çevirmemiz gerekir.
+
+//byte[] message = Encoding.UTF8.GetBytes("Merhaba");
+//channel.BasicPublish(exchange:"" , routingKey: "example-queue",body:message);
+
+for (int i = 0; i < 100; i++)
+{
+    byte[] message = Encoding.UTF8.GetBytes("Merhaba" + i);
+    channel.BasicPublish(exchange: "", routingKey: "example-queue", body: message);
+
+}
+Console.Read();
